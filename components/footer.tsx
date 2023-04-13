25306882946f664ee9fd4f49f21fea23530225f3
foreverfated cookie <MistyJS113@outlook.com>
2023-04-13 05:05:08 +0000

    routes configured

---
+++ components/footer.tsx
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+    return (
+        <div className="w-screen h-max">footer</div>
+    )
+}
---
+++ src/app/home/animelist/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="flex-1">
+      <div className=''>
+        anime list goes here
+      </div>
+    </main>
+  )
+}
